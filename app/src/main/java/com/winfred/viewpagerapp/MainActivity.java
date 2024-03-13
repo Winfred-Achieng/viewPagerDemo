e720d34f88108185199789b2fd3b910344ac1cca
winfred <achiengwinfred20@gmail.com>
2024-03-13 13:23:27 +0300

    tab layout

--- app/src/main/java/com/winfred/viewpagerapp/MainActivity.java
+++ app/src/main/java/com/winfred/viewpagerapp/MainActivity.java
@@ -1,22 +1,46 @@
 package com.winfred.viewpagerapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 public class MainActivity extends AppCompatActivity {
 
     ViewPager2 viewPager;
     ViewPagerAdapter adapter;
+    TabLayout tabLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        viewPager=findViewById(R.id.viewPager2);
+        tabLayout=findViewById(R.id.tabLayout);
 
-//        adapter= new ViewPagerAdapter(getSupportFragmentManager(),getLifecycle())
+        adapter= new ViewPagerAdapter(getSupportFragmentManager(),getLifecycle());
+        adapter.addFragment(new Fragment1());
+        adapter.addFragment(new Fragment2());
+        adapter.addFragment(new Fragment3());
+
+        viewPager=findViewById(R.id.viewPager2);
+        viewPager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+
+        viewPager.setAdapter(adapter);
+
+        //connecting tab layout with viewpager
+        new TabLayoutMediator(tabLayout, viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setText("Fragment " + (position +1));
+            }
+        }
+
+        ).attach();
 
     }
 }
